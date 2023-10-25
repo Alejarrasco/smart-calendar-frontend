bef4a24816ea66a11628ada2858e8d70b6355358
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2023-10-24 21:55:39 -0400

    Connect to API

---
+++ src/components/TestAuth.vue
@@ -0,0 +1,24 @@
+<template>
+    <button>Press Me</button>
+</template>
+
+<script>
+import { useAuth0 } from '@auth0/auth0-vue';
+
+export default {
+    setup() {
+        const { getAccessTokenSilently } = useAuth0();
+
+        return {
+            doSomeThingWithToken: async () => {
+                const token = await getAccessTokenSilently();
+                const response = await fetch('http://localhost:8080/api/v1/protected', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+            }
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,21 @@
     <hero />
     <hr />
     <home-content />
+    <TestAuth />
   </div>
 </template>
 
 <script lang="ts">
 import Hero from "../components/Hero.vue";
 import HomeContent from "../components/HomeContent.vue";
+import TestAuth from "../components/TestAuth.vue";
 
 export default {
   name: "home-view",
   components: {
     Hero,
     HomeContent,
+    TestAuth
   },
 };
 </script>
